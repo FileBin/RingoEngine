fa873b916ef1be1aa265870cfb5bf346b971e85a
filebin <alex.mousse9@gmail.com>
2023-03-31 21:25:28 +0300

    tested hugo and asp.net compatibility

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 var app = builder.Build();
-app.MapGet("/", () => "Hello World!");
+
+var fileProvider = new PhysicalFileProvider(
+       Path.Combine(builder.Environment.ContentRootPath, "public"));
+
+app.UseDefaultFiles(new DefaultFilesOptions {
+    FileProvider = fileProvider
+});
+
+app.UseStaticFiles(new StaticFileOptions {
+    FileProvider = fileProvider
+});
 
 app.Run();
